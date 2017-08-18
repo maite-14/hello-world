3c5636a25fc38e565e791527ac04e45d9f6c0871
quinzel-14 <31133406+quinzel-14@users.noreply.github.com>
2017-08-18 12:23:07 -0400

    Update color_test.java

--- color_test.java
+++ color_test.java
@@ -1,6 +1,17 @@
 import java.io.*;
 
 public class Color{
+  private String name;
+  private double birth_m_y;
+  private int ID_number; 
+  
+  public Identity(String name, double birth_m_y, int ID_number){
+    this.name = name;
+    this.birth_m_y = birth_m_y;
+    this.ID_number = ID_number; 
+    
+    System.out.println("Configuring ID information...");
+  }
   
   public enum Spectrum {RED, BLUE, GREEN, YELLOW, PURPLE, ORANGE}
   
@@ -18,6 +29,10 @@ public class Color{
         
         case GREEN:
           System.out.println("You have the ability of telekinesis.");
+          break;
+        
+        case YELLOW:
+          System.out.println("You have the ability of pyrokinesis.");
           break;
     }
     
