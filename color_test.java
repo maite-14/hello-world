68ade35bd05380e58fb5d7d2349dc3aceb9d2bed
quinzel-14 <31133406+quinzel-14@users.noreply.github.com>
2017-08-18 17:28:46 -0400

    Update color_test.java

--- color_test.java
+++ color_test.java
@@ -1,10 +1,32 @@
-import java.io.*;
+import java.io.*; //import library
 
 public class Color{
+  
+  //max number of IDs 
+  public int max = 8; 
+  
+  //private variables
   private String name;
   private double birth_m_y;
   private int ID_number; 
   
+  //originally set to type Object to permit various data types in the array
+  private int[] primes = {2, 3, 5, 7, 11, 13, 17, 19};
+  
+  //synchronize functions accessing the array 
+  public void setVal(int index, int ID){
+    synchronized(primes){
+      primes[index] = ID; 
+    } 
+  }
+  
+  public int getVal(int index){
+    synchronized(primes){
+      return primes[index]; 
+    }
+  }
+  
+  //function that estabalishes identity through private variables
   public Identity(String name, double birth_m_y, int ID_number){
     this.name = name;
     this.birth_m_y = birth_m_y;
@@ -13,11 +35,14 @@ public class Color{
     System.out.println("Configuring ID information...");
   }
   
-  public enum Spectrum {RED, BLUE, GREEN, YELLOW, PURPLE, ORANGE}
+  //declare an enumeration for various colors
+  public enum Spectrum {RED, BLUE, GREEN, YELLOW, PURPLE}
   
-  public static void main(String args[]){
-    Spectrum choice;
+  //main function
+  public static void main(String[] args){
+    Spectrum choice; //declare an object of Spectrum 
     
+    //switch statement 
     switch(choice){
       case RED:
         System.out.println("You have the ability of superspeed."); 
@@ -34,6 +59,14 @@ public class Color{
         case YELLOW:
           System.out.println("You have the ability of pyrokinesis.");
           break;
+        
+        case PURPLE:
+          System.out.println("You have the ability of super strength."); 
+          break;
+        
+        default:
+          System.out.println("You have no superpowers :(");
+          break; 
     }
     
   }
